89524237c9434b876e22f93398e29555657f31c1
RoyalNavy <minggao92@gmail.com>
2014-05-19 19:47:33 +0000

    Add precompiled assets for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ming Gao"
+    email    "minggao92@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
